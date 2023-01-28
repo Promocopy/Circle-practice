be0adf3a73a242a549c13e844d7fdd0cffe30193
promocopy <tobijosh4@gmail.com>
2023-01-28 19:42:56 +0100

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Program
+{
+    public class Circle
+    {
+        float _Pi = 3.142f;
+        int _radius;
+
+        public Circle(int Radius)
+        {
+            this._radius = Radius;
+        }
+
+        public float calculateArea()
+        {
+            return  this._Pi * this._radius * this._radius;
+        }
+
+
+        public static void Main()
+        {
+            Circle C1 = new Circle(5);
+            float Area = C1.calculateArea();
+
+            Console.WriteLine("Area is {0}", Area);
+        }
+
+      
+       
+    }
+}
